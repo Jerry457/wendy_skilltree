68746087827e00cfb8062312cb921354ba1b2170
jerry <2234834467@qq.com>
2025-03-19 22:50:27 +0800

    update

--- postinit/components/equippable.lua
+++ postinit/components/equippable.lua
@@ -4,10 +4,12 @@ local Equippable = require("components/equippable")
 
 local _GetWalkSpeedMult = Equippable.GetWalkSpeedMult
 function Equippable:GetWalkSpeedMult(...)
+    local speed = _GetWalkSpeedMult(self, ...)
+
     local owner = self.inst.components.inventoryitem and self.inst.components.inventoryitem.owner
-    if self.isequipped and owner and owner:HasTag("vigorbuff") then
-        return 1
+    if speed < 1 and self.isequipped and owner and owner:HasTag("vigorbuff") then
+        speed = math.min(1, speed)
     end
 
-    return  _GetWalkSpeedMult(self, ...)
+    return speed
 end
